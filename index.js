a4301eb11231aecc27afd99b96c16f4cab7cf0e3
joseph <josephmacharia575@gmail.com>
2022-08-23 15:14:45 +0300

    comlpleted assignments

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2 ;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 =4
+let num4 =16
+let mod = num3 % num4;
+
+
+let max = Math.max(8, 12, 20, 15, 4, 10, 6);
+
+
+
